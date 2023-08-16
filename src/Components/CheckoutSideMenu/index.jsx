58b842e3d5a79471206b45911b153ab6522e4f51
Patricia <patriciamvbog@gmail.com>
2023-08-16 15:13:57 +0100

    Function in order to delete a product from the shopping cart

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -6,7 +6,10 @@ import './styles.css'
 
 const CheckoutSideMenu = () => {
     const context = useContext(ShoppingCartContext)
-    console.log('CART: ', context.cartProducts)
+     const handleDelete = (id) => {
+         const filteredProducts = context.cartProducts.filter(product => product.id != id)
+         context.setCartProducts(filteredProducts)
+     }
 
     return (
         <aside
@@ -24,9 +27,11 @@ const CheckoutSideMenu = () => {
                     context.cartProducts.map(product => (
                         <OrderCard
                             key={product.id}
+                            id={product.id}
                             title={product.title}
                             imageUrl={product.images}
                             price={product.price}
+                            handleDelete={handleDelete}
                         />
                     ))
                 }
--- src/Components/OrderCard/index.jsx
+++ src/Components/OrderCard/index.jsx
@@ -1,7 +1,7 @@
 import { XMarkIcon } from '@heroicons/react/24/solid'
 
 const OrderCard = props => {
-    const { title, imageUrl, price } = props
+    const { id, title, imageUrl, price, handleDelete } = props
 
     return (
         <div className="flex justify-between items-center mb-3">
@@ -13,7 +13,10 @@ const OrderCard = props => {
             </div>
             <div className='flex items-center gap-2'>
                 <p className='text-lg font-medium'>€{price}</p>
-                <XMarkIcon className='h-6 w-6 text-black cursor-pointer'></XMarkIcon>
+                <XMarkIcon 
+                className='h-6 w-6 text-black cursor-pointer'
+                onClick={() => handleDelete(id)}
+                ></XMarkIcon>
             </div>
         </div>
     )
